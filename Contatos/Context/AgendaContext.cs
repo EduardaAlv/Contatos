0621695eba4790bbdfd1d07a861d19a92229b12d
EduardaAlv <eduardasantosalveseduarda@gmail.com>
2023-01-26 22:22:13 -0300

    Add project files.

---
+++ Contatos/Context/AgendaContext.cs
@@ -0,0 +1,15 @@
+﻿using Contatos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contatos.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
